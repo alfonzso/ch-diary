b4908dbeaa0e549acfd36bf592292c0fef0798f2
Zsolt Alföldi <zsolt.alfoldi@icellmobilsoft.hu>
2022-06-30 04:01:58 +0200

    done, login next

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,6 @@ function App() {
 
   return (
     <div className="App">
-
       <Routes>
         <Route path="/" element={<Navbar />}>
           <Route index element={<Home />} />
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import "./navbar.css"
-import { Link, Outlet } from "react-router-dom";
-import { UserData } from '../types';
+import { useState } from "react";
 import { connect } from 'react-redux';
+import { Link, Outlet } from "react-router-dom";
 import { Dispatch } from "redux";
 import { RootState } from '../redux/store';
+import { UserData } from '../types';
+import "./navbar.scss";
 
 interface NavbarProps {
   // userData: userData
@@ -16,61 +16,71 @@ interface NavbarState {
 
 }
 
-class Navbar extends React.Component<NavbarProps, NavbarState> {
-  // state = { :  }
+// class Navbar extends React.Component<NavbarProps, NavbarState> {
+// state = { :  }
+const Navbar = ({ user }: NavbarProps) => {
 
-  render() {
-    // const { userData } = this.props;
-    return (
-      <div>
-        <nav className="navigation">
-          <a href="/" className="brand-name">
-            Ch Diary ={">"} {this.props.user.nickname}
-          </a>
-          <button className="hamburger">
-            {/* icon from heroicons.com */}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              viewBox="0 0 20 20"
-              fill="white"
-            >
-              <path
-                fillRule="evenodd"
-                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </button>
-          <div
-            className="navigation-menu">
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/today">Today</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/login">Login</Link>
-              </li>
-              <li>
-                <Link to="/register">Register</Link>
-              </li>
-              <li>
-                <Link to="/test">Test</Link>
-              </li>
-            </ul>
-          </div>
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
+  // render() {
+  // return (
+  // const { userData } = this.props;
 
-        </nav>
-        <Outlet />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <nav className="navigation">
+        <a href="/" className="brand-name">
+          Ch Diary ={">"} {user.nickname}
+        </a>
+        <button className="hamburger"
+          onClick={() => {
+            setIsNavExpanded(!isNavExpanded);
+          }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="white"
+          >
+            <path
+              fillRule="evenodd"
+              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
+        <div
+          className={
+            isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
+          }
+        >
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/today">Today</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+            <li>
+              <Link to="/test">Test</Link>
+            </li>
+          </ul>
+        </div>
+
+      </nav>
+      <Outlet />
+    </div>
+  );
+  // )
 }
 
 // export default Navbar;
