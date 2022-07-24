4fe604a18784955311adc2189d40f7c75079b1fe
Zsolt Alföldi <zsolt.alfoldi@icellmobilsoft.hu>
2022-07-25 01:12:35 +0200

    .

--- src/Hooks/useFetch.ts
+++ src/Hooks/useFetch.ts
@@ -62,94 +62,30 @@ export const fetchWrapper = (
 const useFetch = <T>(url: string, config: RequestInit = {}) => {
   const [data, setData] = useState([] as unknown as TestResponse);
 
-
-  // fetchAgainWithGoodToken
   const retryFetchWithNewToken = () => {
-    // return fetch(`${baseURL}/api/auth/refreshToken`, { method: 'GET', credentials: 'include', })
-    //   .then((response) => response.json())
-    //   .then((refreshTokenResponse: TokenResponse) => {
-    //     if (refreshTokenResponse.error) throw new Error(JSON.stringify(refreshTokenResponse.error))
-    //     inMemoryJwt.setToken(refreshTokenResponse.accessToken)
-    //   })
-
     const resolve = (refreshTokenResponse: TokenResponse) => {
       if (refreshTokenResponse.error) throw new Error(JSON.stringify(refreshTokenResponse.error))
       inMemoryJwt.setToken(refreshTokenResponse.accessToken)
     }
-    // const reject = (err: Error) => {
-    //   console.log(err)
-    // }
     return fetchWrapper("/api/auth/refreshToken", resolve, undefined, { method: 'GET', credentials: 'include', })
   }
-  // type voidOr = (res: T) => T | void
 
   const newFetch = <T extends ResponseErrorHandler>(newFetchResolve: (res: T) => T | void, reject?: () => any) => {
-    // const fixedUrl = url.includes('https') ? url : `${baseURL}${url}`
-    // const fetchTest = fetch.bind(null, fixedUrl, setConfigHeader(config, inMemoryJwt.getToken()))
-    // fetch(fixedUrl, setConfigHeader(config, inMemoryJwt.getToken()))
-    // fetchTest()
-    //   .then(async (response) => {
-    //     console.log("response.ok: ", response.ok)
-    //     if (response.ok) {
-    //       return response.json();
-    //     }
-    //     // throw new Error(response.status);
-    //     // let refreshTokenResponse = await fetch(
-    //     //   `${baseURL}/api/auth/refreshToken`,
-    //     //   { method: 'GET', credentials: 'include', }
-    //     // )
-    //     // console.log("refreshTokenResponse ", refreshTokenResponse);
-    //     // let refTokenAccesToken = await refreshTokenResponse.json() as accessTokenAndRefreshToken
-    //     // inMemoryJwt.setToken(refTokenAccesToken.accessToken)
-
-    //     await fetchAgainWithGoodToken()
-
-    //     // let newResponse = await fetch(fixedUrl, setConfigHeader(config, inMemoryJwt.getToken()))
-    //     let newResponse = await fetchTest()
-    //     console.log("newResponse.ok: ", newResponse.ok)
-    //     if (newResponse.ok) {
-    //       return response.json();
-    //     }
-    //   })
-    //   .then((data) => setData(data))
-    //   .catch(err => {
-    //     console.log(err);
-    //   })
 
     const firstFetchResolve = async (response: T) => {
-      // if (response.error) throw new Error(JSON.stringify(response.error))
-      // inMemoryJwt.setToken(refreshTokenResponse.accessToken)
       console.log("firstFetchResolve response.ok: ", response, response.error ? 1 : 2)
       if (response.error) {
         await retryFetchWithNewToken()
-
-        // fetchWrapper(url, (response: T) => {
-        //   if (response.error) throw new Error(JSON.stringify(response.error))
-        //   return response
-        // }, undefined, setConfigHeader(config, inMemoryJwt.getToken()))
         fetchWrapper(url, newFetchResolve, undefined, setConfigHeader(config, inMemoryJwt.getToken()))
 
       } else {
         console.log('resolve(response)');
         return newFetchResolve(response)
       }
-      // if (response.ok) {
-      //   return response.json();
-      // }
     }
-    // const reject = (err: Error) => {
-    //   console.log(err)
-    // }
     fetchWrapper(url, firstFetchResolve, undefined, setConfigHeader(config, inMemoryJwt.getToken()))
 
   }
-
-  // newFetch<TestResponse>(
-  //   (data) => {
-  //     console.log("setData(data) ", data)
-  //     setData(data)
-  //   }
-  // )
 
   useEffect(() => {
     newFetch<TestResponse>(
@@ -158,15 +94,6 @@ const useFetch = <T>(url: string, config: RequestInit = {}) => {
         setData(data)
       }
     )
-    // newFetch<TestResponse>(
-    //   (data) => {
-    //     setData(data)
-    //     // return data
-    //   }
-    // )
-    // .catch(err => {
-    //   console.log("err ", err)
-    // });
   }, []);
 
   return data;
