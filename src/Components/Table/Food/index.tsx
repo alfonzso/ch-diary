6502071871dcc0401d4ce20840669fdbf167dceb
Zsolt Alföldi <zsolt.alfoldi@icellmobilsoft.hu>
2022-08-11 00:36:32 +0200

    added popupInfo

--- src/Components/Table/Food/index.tsx
+++ src/Components/Table/Food/index.tsx
@@ -28,8 +28,6 @@ const FoodProperiteComponent = (
 }
 
 const Food = ({ food, setInitFollowers }: FoodProps) => {
-  const popupRef = useRef<HTMLDivElement>(null);
-
   const [hundredGrammToggle, setHundredGrammToggle] = useState(false)
   const [calculatedCh, setCalculatedCh] = useState(0.0)
   const [hundredGrammValues, setHundredGrammValues] = useState("")
@@ -70,53 +68,81 @@ const Food = ({ food, setInitFollowers }: FoodProps) => {
 
   return food ? (
     <div id={"food_" + food.id} data-id={food.id} className="food" draggable="true" onDragStart={drag} >
-      <p className="foodInfoText" > {food.portion} g {food.name} {food.type} </p>
-      < div id="foodProp" className="follower" >
-        <div className="followerMenuBar">
-          <p className="followerFoodText" > {food.name} </p>
-          <div className="toggleVisibility" onClick={() => { setHundredGrammToggle(!hundredGrammToggle) }}> P </div>
-        </div>
-        {hundredGrammToggle && <p> {hundredGrammValues} </p>}
-        <FoodProperiteComponent foodProps={food.props} portion={food.portion} />
-      </div >
-      < div id="diabInfo" className="follower" >
-        <p> CH: {calculatedCh.toFixed(2)} </p>
-        <p> Insulin ratio: {userData.chInsulinRatio} </p>
-        <>
-          <div className="insulin-ratio-and-need">
-            <p className="p-left"> Ratio </p> <p className="p-right"> Insulin needed </p>
-          </div>
-          {insulinNeeded.map((v, idx) => {
-            return (
-              <div key={idx} className="insulin-ratio-and-need">
-                <p className="p-left">  {v.insulinRatio} </p> <p className="p-right"> {v.insulin.toFixed(2)}</p>
-              </div>
-            )
-          })
-          }
-        </>
+      <p className="food-info-text" > {food.portion} g {food.name} {food.type} </p>
 
-      </div >
-      <div className="popup"
-        ref={popupRef}
-        style={{
-          top: popupRef.current?.offsetHeight ? (popupRef.current?.offsetHeight! + 5) * -1 : "unset",
-          left: 10,
-          backgroundColor: "grey"
-        }}
-      >
-        <div className="followerMenuBar">
-          <p className="followerFoodText" > {food.name} </p>
-          <div className="toggleVisibility" onClick={() => { setHundredGrammToggle(!hundredGrammToggle) }}> P </div>
-        </div>
-        {hundredGrammToggle && <p> {hundredGrammValues} </p>}
-        <FoodProperiteComponent foodProps={food.props} portion={food.portion} />
-      </div>
+      <PopupInfo clsName="food-info" backgroundColor="grey" fontSize="75%" width="150px">
+        <>
+          <div className="fi-menu-bar">
+            <p className="fi-food-name" > {food.name} </p>
+            <div className="fi-toggle-visibility" onClick={() => { setHundredGrammToggle(!hundredGrammToggle) }}> P </div>
+          </div>
+          {hundredGrammToggle && <p> {hundredGrammValues} </p>}
+          <FoodProperiteComponent foodProps={food.props} portion={food.portion} />
+        </>
+      </PopupInfo>
+
+      <PopupInfo clsName="diab-info" left="400px" backgroundColor="#228ba5" fontSize="75%" width="150px">
+        <>
+          <p> CH: {calculatedCh.toFixed(2)} </p>
+          <p> Insulin ratio: {userData.chInsulinRatio} </p>
+          <>
+            <div className="insulin-ratio-and-need">
+              <p className="p-left"> Ratio </p> <p className="p-right"> Insulin needed </p>
+            </div>
+            {insulinNeeded.map((v, idx) => {
+              return (
+                <div key={idx} className="insulin-ratio-and-need">
+                  <p className="p-left">  {v.insulinRatio} </p> <p className="p-right"> {v.insulin.toFixed(2)}</p>
+                </div>
+              )
+            })
+            }
+          </>
+        </>
+      </PopupInfo>
+
     </div >
   ) : null
 }
 
+
+type PopupInfoProps = {
+  children: JSX.Element
+  clsName?: string
+  left?: string
+  backgroundColor?: string
+  fontSize?: string
+  width?: string
+  padding?: string
+}
+
+function PopupInfo({ children, clsName, left, backgroundColor, fontSize, width, padding }: PopupInfoProps) {
+  const popupRef = useRef<HTMLDivElement>(null);
+
+  return (
+    <div
+      className={clsName ? clsName + " popup" : "popup"}
+      ref={popupRef}
+      style={
+        {
+          top: popupRef.current?.offsetHeight ? (popupRef.current?.offsetHeight! + 5) * -1 : "unset",
+          left: left ? left : 10,
+          "--popup-color": backgroundColor ? backgroundColor : "black",
+          fontSize: fontSize ? fontSize : "100%",
+          width: width ? width : "100%",
+          padding: padding ? padding : "10px 0 10px 0"
+        } as React.CSSProperties
+      }
+    >
+      {children}
+    </div>
+  );
+}
+
+export default PopupInfo;
+
 export {
   Food
 }
+
 export type { FoodProps, foodInnerProps }
